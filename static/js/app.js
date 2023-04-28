6d51ece614fe90943e3acbe2ed7557333d69ed4c
MSF2141 <114106019+MSF2141@users.noreply.github.com>
2023-04-27 21:04:47 -0400

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -24,27 +24,27 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-var filters = {};
+var Filters = {};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-    let changedElement = d3.select(this);
+    let element = d3.select(this);
     // 4b. Save the value that was changed as a variable.
-    let elementValue = changedElement.property("value");
-    console.log(elementValue);
+    let elementValue = element.property("value");
     // 4c. Save the id of the filter that was changed as a variable.
-    let filteredId = changedElement.attr("id");
-    console.log(filterId);
+    let elementId = element.attr("id");
+  
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
-      filters[filterId] = elementValue;
+      Filters[elementId] = elementValue;
     }
     else {
-      delete filters[filterId];
-    } 
+      delete Filters[elementId];
+    }
+  
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -58,9 +58,13 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.entries(Filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
+  
+  
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
